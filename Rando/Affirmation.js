ed6197e370e0c77aea2cea2bd2c72448c0e341ba
LowBurner <111740316+LowBurner@users.noreply.github.com>
2023-05-17 07:25:00 +1000

    put the messages in a object

--- Rando/Affirmation.js
+++ Rando/Affirmation.js
@@ -1,4 +1,6 @@
-const youAre = ['a strong and powerful person', 'a wise and kind soul', 'a resilient and patient person' ]
-const youHave = ['the strength to persist', 'the courage to be vulnerable', 'the patience of a saint' ]
-const youWill = ['not only survive, but also thrive', 'be the difference in someones life', 'shine and illuminate during dark days' ]
-//console.log(youAre[0])youHave
+const affirmPhrase = {
+youAre: ['a strong and powerful person', 'a wise and kind soul', 'a resilient and patient person' ],
+youHave: ['the strength to persist', 'the courage to be vulnerable', 'the patience of a saint' ],
+youWill: ['not only survive, but also thrive', 'be the difference in someones life', 'shine and illuminate during dark days' ],
+}
+console.log(affirmPhrase.youAre)
