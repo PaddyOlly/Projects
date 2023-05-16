4e422e009869c0a50ece27eb99625cb1d16bfdf0
LowBurner <111740316+LowBurner@users.noreply.github.com>
2023-05-17 07:14:23 +1000

    messages set in var's

--- Rando/Affirmation.js
+++ Rando/Affirmation.js
@@ -1,5 +1,4 @@
-console.log('this works')
-console.log('this is something too')
-console.log('now')
-console.log('1123')
-console.log('new something')
+const youAre = ['a strong and powerful person', 'a wise and kind soul', 'a resilient and patient person' ]
+const youHave = ['the strength to persist', 'the courage to be vulnerable', 'the patience of a saint' ]
+const youWill = ['not only survive, but also thrive', 'be the difference in someones life', 'shine and illuminate during dark days' ]
+//console.log(youAre[0])youHave
