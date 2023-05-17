5366d860db749b733f528d5668235ac334d690ff
LowBurner <111740316+LowBurner@users.noreply.github.com>
2023-05-18 07:16:46 +1000

    Made the radom number generator

--- Rando/Affirmation.js
+++ Rando/Affirmation.js
@@ -1,6 +1,16 @@
+// generate random numbers to select random messages
+let randNumOne = Math.floor(Math.random()*3) + 1;
+let randNumTwo = Math.floor(Math.random()*3) + 1;
+let randNumThree = Math.floor(Math.random()*3) + 1;
 const affirmPhrase = {
 youAre: ['a strong and powerful person', 'a wise and kind soul', 'a resilient and patient person' ],
 youHave: ['the strength to persist', 'the courage to be vulnerable', 'the patience of a saint' ],
 youWill: ['not only survive, but also thrive', 'be the difference in someones life', 'shine and illuminate during dark days' ],
+
+
 }
-console.log(affirmPhrase.youAre)
+
+console.log(randNumOne)
+console.log(randNumTwo)
+console.log(randNumThree)
+//console.log(affirmPhrase.youAre)
